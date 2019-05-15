13d3ce409e72ebc6d104fbae0dcc260306a2390c
Jay Maas <jaymaas.dev@gmail.com>
2019-05-15 01:10:41 -0600

    Beautify
    formatted two pages for readability

--- redux-todo/src/components/Title.js
+++ redux-todo/src/components/Title.js
@@ -1,20 +1,28 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { updateTitle } from '../actions'
+import {
+    connect
+} from 'react-redux'
+import {
+    updateTitle
+} from '../actions'
 import seaStar from '../assets/images/seaStar.png'
 
 class Title extends React.Component {
-    state={
+    state = {
         newTitle: this.props.title,
         editing: false
     }
     handleChanges = e => {
-        this.setState({ [e.target.name]: e.target.value })
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
     updateTitle = e => {
         e.preventDefault()
         this.props.updateTitle(this.state.newTitle)
-        this.setState({ editing: false })
+        this.setState({
+            editing: false
+        })
     }
     render() {
         // let input = document.querySelector('input'); // get the input element
@@ -24,29 +32,61 @@ class Title extends React.Component {
         // function resizeInput() {
         //     this.style.width = this.value.length + "ch";
         // }
-        return (
-            <div style={{position: 'relative'}}>
-                {!this.state.editing ? (
-                    <h1 
-                    style={{ textShadow: '11px 4px 10px black',
-                    fontSize: '3em',
-                    margin: '0'
-                    }}
-                    onClick={() => this.setState({ editing: true })}>{this.props.title}</h1>
-                ) : (
-                    <>
-                       <input
-                            className="Input"
-                            type="text"
-                            name="newTitle"
-                            value={this.state.newTitle}
-                            onChange={this.handleChanges}
-                        />
-                        <img src={seaStar} alt='seaStart' style={{position: 'absolute',top: '0', 
-                        right: '0', width: '20%', height: 'auto'}} onClick={this.updateTitle}/>
-                    </>
-                )}
-            </div>
+        return ( <
+            div style = {
+                {
+                    position: 'relative'
+                }
+            } > {
+                !this.state.editing ? ( <
+                    h1 style = {
+                        {
+                            textShadow: '11px 4px 10px black',
+                            fontSize: '3em',
+                            margin: '0'
+                        }
+                    }
+                    onClick = {
+                        () => this.setState({
+                            editing: true
+                        })
+                    } > {
+                        this.props.title
+                    } < /h1>
+                ) : ( <
+                    >
+                    <
+                    input className = "Input"
+                    type = "text"
+                    name = "newTitle"
+                    value = {
+                        this.state.newTitle
+                    }
+                    onChange = {
+                        this.handleChanges
+                    }
+                    /> <
+                    img src = {
+                        seaStar
+                    }
+                    alt = 'seaStart'
+                    style = {
+                        {
+                            position: 'absolute',
+                            top: '0',
+                            right: '0',
+                            width: '20%',
+                            height: 'auto'
+                        }
+                    }
+                    onClick = {
+                        this.updateTitle
+                    }
+                    /> <
+                    />
+                )
+            } <
+            /div>
         )
     }
 }
@@ -58,6 +98,7 @@ const mapStateToProps = state => {
 }
 
 export default connect(
-    mapStateToProps,
-    { updateTitle }
+    mapStateToProps, {
+        updateTitle
+    }
 )(Title)
--- redux-todo/src/components/Todos.js
+++ redux-todo/src/components/Todos.js
@@ -1,99 +1,195 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { addTodo, toggleCompleted, deleteTodo } from '../actions'
+import {
+    connect
+} from 'react-redux'
+import {
+    addTodo,
+    toggleCompleted,
+    deleteTodo
+} from '../actions'
 import corona from '../assets/images/corona.png'
 import coronaReverse from '../assets/images/coronaReverse.png'
 import bobMarley from '../assets/images/bobMarley.png'
 
 class Todos extends React.Component {
-    state = {
-        newTodo: ''
-    }
-    handleChanges = e => {
-        this.setState({ newTodo: e.target.value })
-    }
-    addTodo = e => {
-        e.preventDefault()
-        this.props.addTodo(this.state.newTodo)
-        this.setState({ newTodo: '' })
-    }
-    toggleCompleted = id => {
-        this.props.toggleCompleted(id)
-    }
-    deleteTodo = id => {
-        this.props.deleteTodo(id)
-    }
-    beerToggle = () => this.props.todos && this.props.todos.filter(todo => {
-        return todo.completed === true
-    }) 
-    render() {
-        let beerToggle = this.beerToggle()
-        beerToggle = beerToggle.length === this.props.todos.length
-        let bobToggle = this.props.todos.length === 0
-        console.log(bobToggle)
-        return (
-            <>
-                {this.props.todos && this.props.todos.map(todo => (
-                    <div 
-                    className="Todo"
-                    onMouseEnter={() => document.getElementById(`${todo.id}`).style.display = "initial"}
-                    onMouseLeave={() => document.getElementById(`${todo.id}`).style.display = "none"}
-                    key={todo.id} 
-                    style={{
-                        width: '100%%', 
-                        display: 'flex', 
-                        position: 'relative'}}
+        state = {
+            newTodo: ''
+        }
+        handleChanges = e => {
+            this.setState({
+                newTodo: e.target.value
+            })
+        }
+        addTodo = e => {
+            e.preventDefault()
+            this.props.addTodo(this.state.newTodo)
+            this.setState({
+                newTodo: ''
+            })
+        }
+        toggleCompleted = id => {
+            this.props.toggleCompleted(id)
+        }
+        deleteTodo = id => {
+            this.props.deleteTodo(id)
+        }
+        beerToggle = () => this.props.todos && this.props.todos.filter(todo => {
+            return todo.completed === true
+        })
+        render() {
+            let beerToggle = this.beerToggle()
+            beerToggle = beerToggle.length === this.props.todos.length
+            let bobToggle = this.props.todos.length === 0
+            console.log(bobToggle)
+            return ( <
+                > {
+                    this.props.todos && this.props.todos.map(todo => ( <
+                        div className = "Todo"
+                        onMouseEnter = {
+                            () => document.getElementById(`${todo.id}`).style.display = "initial"
+                        }
+                        onMouseLeave = {
+                            () => document.getElementById(`${todo.id}`).style.display = "none"
+                        }
+                        key = {
+                            todo.id
+                        }
+                        style = {
+                            {
+                                width: '100%%',
+                                display: 'flex',
+                                position: 'relative'
+                            }
+                        } >
+                        <
+                        div >
+                        <
+                        p style = {
+                            {
+                                textDecoration: `${todo.completed ? 'line-through' : 'none'}`,
+                                width: '100%',
+                                cursor: 'pointer',
+                                textShadow: '11px 4px 10px black',
+                                fontSize: '1.6em'
+                            }
+                        }
+                        onClick = {
+                            () => this.toggleCompleted(todo.id)
+                        } >
+                        {
+                            todo.todo
+                        } <
+                        /p> <
+                        /div> <
+                        span id = {
+                            todo.id
+                        }
+                        style = {
+                            {
+                                fontSize: '1.6em',
+                                display: 'none',
+                                cursor: 'no-drop',
+                                position: 'absolute',
+                                right: '0',
+                                bottom: '50%'
+                            }
+                        }
+                        onClick = {
+                            () => this.deleteTodo(todo.id)
+                        } >
+                        x <
+                        /span> <
+                        /div>
+                    ))
+                } {
+                    this.props.todos.length < 3 ?
+                        <
+                        div >
+                        <
+                        form onSubmit = {
+                            this.addTodo
+                        } >
+                        <
+                        input
+                    className = "InputTodo"
+                    type = "text"
+                    value = {
+                        this.state.newTodo
+                    }
+                    onChange = {
+                        this.handleChanges
+                    }
+                    placeholder = "Add New Todo" /
                         >
-                        <div>
-                            <p style={{
-                            textDecoration: `${todo.completed ? 'line-through' : 'none'}`, 
-                            width: '100%', 
-                            cursor: 'pointer',
-                            textShadow: '11px 4px 10px black',
-                            fontSize: '1.6em'}} 
-                            onClick={() => this.toggleCompleted(todo.id)}
-                            >
-                                {todo.todo}
-                            </p>
-                        </div>
-                        <span id={todo.id} style={{ fontSize: '1.6em', display: 'none', cursor: 'no-drop', position: 'absolute', right: '0', bottom: '50%'}}onClick={() => this.deleteTodo(todo.id)}
-                        >
-                            x
-                        </span>
-                    </div>
-                ))}
-                {this.props.todos.length < 3 ? 
-                    <div>
-                        <form onSubmit={this.addTodo}>
-                            <input
-                                className="InputTodo"
-                                type="text"
-                                value={this.state.newTodo}
-                                onChange={this.handleChanges}
-                                placeholder="Add New Todo"
-                            />
-                        </form>
-                        <button onClick={this.addTodo}>Add Todo</button>
-                    </div> : <><p style={{fontSize: '1em', width: '75%', textShadow: 'none', filter: 'invert(1)', fontWeight: '800'}}>You can only have 3 todos on this app, because you are at the beach, and you should relax...Try completing all your todos or deleting them!</p><img style={{position: 'absolute', left: '0', bottom: '0', maxHeight: '80vh'}}src={corona} alt="corona"/>
-                    
-                    </>
+                        <
+                        /form> <
+                        button onClick = {
+                            this.addTodo
+                        } > Add Todo < /button> <
+                        /div> : <><p style={{fontSize: '1em', width: '75%', textShadow: 'none', filter: 'invert(1)', fontWeight: '800'}}>You can only have 3 todos on this app, because you are at the beach, and you should relax...Try completing all your todos or deleting them!</p > < img style = {
+                            {
+                                position: 'absolute',
+                                left: '0',
+                                bottom: '0',
+                                maxHeight: '80vh'
+                            }
+                        }
+                    src = {
+                        corona
+                    }
+                    alt = "corona" / >
+
+                        <
+                        />
+                } {
+                    beerToggle ? < img style = {
+                        {
+                            position: 'absolute',
+                            right: '0',
+                            bottom: '0',
+                            maxHeight: '80vh'
+                        }
+                    }
+                    src = {
+                        coronaReverse
+                    }
+                    alt = "corona" / >: null
                 }
-                {beerToggle ? <img style={{position: 'absolute', right: '0', bottom: '0', maxHeight: '80vh'}}src={coronaReverse} alt="corona"/> : null }
 
-                {bobToggle ? <><img style={{position: 'absolute', left: '0', bottom: '0', maxHeight: '80vh'}}src={bobMarley} alt="bobMarley"/> <embed 
-                            style={{ opacity: '0'}}
-                            src= "https://a.tumblr.com/tumblr_lr49ls0X7L1qej9iso1.mp3"
-                            loop="false"
-                            autostart="true"
-                            mastersound/> </>: null }
-            </>
-        )
-    }
-}
-const mapStateToProps = state => {
-    return {
-        todos: state.todos
-    }
-}
+                {
+                    bobToggle ? < > < img style = {
+                        {
+                            position: 'absolute',
+                            left: '0',
+                            bottom: '0',
+                            maxHeight: '80vh'
+                        }
+                    }
+                    src = {
+                        bobMarley
+                    }
+                    alt = "bobMarley" / > < embed
+                    style = {
+                        {
+                            opacity: '0'
+                        }
+                    }
+                    src = "https://a.tumblr.com/tumblr_lr49ls0X7L1qej9iso1.mp3"
+                    loop = "false"
+                    autostart = "true"
+                    mastersound / > < />: null } <
+                        />
+                )
+            }
+        }
+        const mapStateToProps = state => {
+            return {
+                todos: state.todos
+            }
+        }
 
-export default connect(mapStateToProps, { addTodo, toggleCompleted, deleteTodo })(Todos)
+        export default connect(mapStateToProps, {
+            addTodo,
+            toggleCompleted,
+            deleteTodo
+        })(Todos)
